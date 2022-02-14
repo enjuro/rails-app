a0b09b6102646225f77a3bfd886e9eff71d4f6b6
enjuro <enjuro0926@gmail.com>
2022-02-14 09:43:55 +0000

    added index to model and fixed fav

--- db/migrate/20220214005754_create_favorites.rb
+++ db/migrate/20220214005754_create_favorites.rb
@@ -5,6 +5,10 @@ class CreateFavorites < ActiveRecord::Migration[5.2]
       t.integer :post_image_id
 
       t.timestamps
+
+      add_index :favorites, :user_id
+      add_index :favorites, :post_image_id
+      add_index :favorites, [:user_id, :post_image_id], unique: true
     end
   end
 end
