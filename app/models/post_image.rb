60d3ad730621db9bceb38b7aa205dae14b16e644
enjuro <enjuro0926@gmail.com>
2022-02-16 07:12:52 +0000

    added validations to user admin post_image models

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,12 @@ class PostImage < ApplicationRecord
     has_many :post_comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
 
+    validates :image, presence: true
+    validates :caption, presence: true
+    validates :grade, presence: true
+    validates :color, presence: true
+    validates :wall_type, presence: true
+
     def favorited_by?(user)
         favorites.where(user_id: user.id).exists?
     end
