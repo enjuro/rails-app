8fa5e5a14029d44c8219a35e59088fdc761f0739
enjuro <enjuro0926@gmail.com>
2022-03-01 05:00:11 +0000

    responsive list

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,9 @@
 class HomesController < ApplicationController
     def top
-
-        relationship = Relationship.where(user_id: current_user.id).order(created_at: :desc).pluck(:admin_id)
-        @followees = Admin.find(relationship)
-
+        if user_signed_in?
+            relationship = Relationship.where(user_id: current_user.id).order(created_at: :desc).pluck(:admin_id)
+            @followees = Admin.find(relationship)
+        end
     end
 
     def links
